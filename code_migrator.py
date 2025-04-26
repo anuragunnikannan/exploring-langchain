a982894b391bc61429cbe39f3df86f97e33bbeb1
anuragunnikannan <anuragunnikannan7@gmail.com>
2025-04-26 18:11:22 +0530

    added code for generating tsd using chunking strategy

--- code_migrator.py
+++ code_migrator.py
@@ -3,9 +3,7 @@ from langchain_ollama import ChatOllama, OllamaLLM
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema.output_parser import StrOutputParser
-
-llm = ChatOllama(model="llama3.2", temperature=0.3,
-                 base_url=os.environ.get("BASE_URL"))
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 BASE_DIR = "/media/anurag/mybkp/coderrank/coderrank-service"
 
@@ -28,6 +26,71 @@ def get_content(filepath):
         content = f.read()
 
     return content
+
+
+def write_code_to_text_file():
+    files = get_all_files(
+        ignore_files=["__pycache__", ".git", "README.md", "venv", "docker-compose.yml", "nginx.conf", ".gitignore", "Dockerfile_nginx"])
+
+    text = ""
+    for file in files:
+        text += f"\n\nFilename: {file}\n\n"
+        text += get_content(file)
+        text += "\n\n----------------------------------\n\n"
+
+    with open('project_source_code.txt', 'w') as f:
+        f.write(text)
+
+
+def split_text(text, chunk_size=4000, overlap=200):
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=overlap)
+
+    return splitter.split_text(text)
+
+
+def generate_tsd_from_chunk(llm, chunk):
+    template = """
+        You are a senior software architect, experienced in working in enterprise level projects. Based on the given codebase, create a technical specification document including modules, classes, DB tables, APIs and external dependencies. You should go through the snippet thoroughly before writing the document.
+    """
+
+    prompt_template = ChatPromptTemplate(
+        [("system", template), ("human", "{chunk}")], format=None)
+
+    chain = prompt_template | llm
+
+    return chain.invoke({"chunk": chunk})
+
+
+def generate_tsd():
+
+    write_code_to_text_file()
+
+    llm = ChatOllama(model="phi4", temperature=0.3,
+                     base_url=os.environ.get("BASE_URL"))
+
+    code = ""
+    with open("project_source_code.txt", "r") as f:
+        code = f.read()
+
+    chunks = split_text(code)
+
+    partial_tsds = [generate_tsd_from_chunk(llm, chunk) for chunk in chunks]
+    combined_summary = "\n\n".join(partial_tsds)
+
+    template = """
+        You are an expert software developer with ample amount of experience working in enterprise level projects. Your task is to refine and consolidate the following technical specification documents without adding or removing any information.
+    """
+
+    prompt_template = ChatPromptTemplate(
+        [("system", template), ("human", "{tsds}")])
+
+    chain = prompt_template | llm
+
+    final_tsd = chain.invoke({"tsds": combined_summary})
+
+    with open("final_tsd.txt", "w") as f:
+        f.write(final_tsd)
 
 
 def migrate_code():
@@ -75,4 +138,4 @@ def migrate_code():
 
 
 if __name__ == "__main__":
-    migrate_code()
+    generate_tsd()
